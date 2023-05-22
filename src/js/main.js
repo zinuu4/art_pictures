eafb0caffbf8845191f33a63e84731f2a89d988b
zinuu4 <unvm7721@gmail.com>
2023-05-22 16:22:09 +0300

    implemented script for sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import modals from "./modules/modals";
+import slider from "./modules/sliders";
+import sliders from "./modules/sliders";
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
   
   modals();
+  sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn', 3000);
+  sliders('.main-slider-item', 'vertical', '', '', 3000);
 });
