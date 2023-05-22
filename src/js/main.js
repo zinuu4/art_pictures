dbcf107618ec060878b4b2b0170a7b435e3646c9
zinuu4 <unvm7721@gmail.com>
2023-05-22 21:16:28 +0300

    implemented sending forms script

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
 import modals from "./modules/modals";
-import slider from "./modules/sliders";
 import sliders from "./modules/sliders";
+import forms from "./modules/forms";
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
+
+  new WOW().init();
   
   modals();
   sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn', 3000);
   sliders('.main-slider-item', 'vertical', '', '', 3000);
+  forms();
 });
